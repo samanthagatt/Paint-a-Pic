7a7f7ee2d96641214b07591597ed766dd884966e
Samantha Gatt <smfgatt@gmail.com>
2021-07-02 20:37:23 -0400

    Pushing what was untracked

--- Nonograms/Utils/Tutorial/Tutorials.swift
+++ Nonograms/Utils/Tutorial/Tutorials.swift
@@ -19,18 +19,18 @@ let tutorials = [
                           rowClues: [[5]],
                           colClues: [[1], [1], [1], [1], [1]]),
              instruction: """
-                The clues on the edges of the puzzle represent how many squares \
-                should be filled in sequentially.\n
-                If there is only one clue for a row or column and it is equal \
-                to the total number of squares in that direction, you can fill \
-                all the squares in.
+                The clues on the edges of the puzzle represent how many \
+                squares should be filled in sequentially.\n
+                If there is only one clue for a row or column and it is \
+                equal to the total number of squares in that direction, you \
+                can fill all the squares in.
                 """),
     Tutorial(clues: .init(name: "tut-2",
                           rowClues: [[2, 2], [1, 1, 1]],
                           colClues: [[2], [1], [1], [1], [2]]),
              instruction: """
-                If there is more than one clue for a row or column, there must \
-                be at least one empty square between them.\n
+                If there is more than one clue for a row or column, there \
+                must be at least one empty square between them.\n
                 If the sum of the clues plus one in between each is equal \
                 to the total count of squares in that direction, there is \
                 only one possible configuration.
